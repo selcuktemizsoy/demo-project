2b3fa662c242242ee03db98c5807f936c12c8555
Selçuk <selcuktemizsoy@gmail.com>
2021-03-01 16:35:50 +0300

    Update UsersRunner.java

--- src/test/java/examples/users/UsersRunner.java
+++ src/test/java/examples/users/UsersRunner.java
@@ -2,7 +2,7 @@ package examples.users;
 
 import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
+
 
 @RunWith(Karate.class)
 @CucumberOptions(
